a93a8fcec864367fc4943ca49686a75faee1ca48
Palidan MAIMAITI <pmaimait@made-f0Br3s7.clusters.42paris.fr>
2023-12-12 10:07:19 +0100

    ex02 main

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmaimait <pmaimait@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 10:30:07 by pmaimait          #+#    #+#             */
-/*   Updated: 2023/12/11 14:24:00 by pmaimait         ###   ########.fr       */
+/*   Updated: 2023/12/12 10:05:17 by pmaimait         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int main()
         std::list<int> s(list);
     }
     
-     std::cout << "-----------vector-------------------\n";
+    std::cout << "-----------vector-------------------\n";
 
     {
         std::vector<int> vector;
@@ -87,6 +87,40 @@ int main()
             ++it;
         }
         std::vector<int> s(vector);
+    }
+
+     std::cout << "-----------test with vector-------------------\n";
+
+    {
+        std::vector<int> vector;
+        vector.push_back(5);
+        vector.push_back(17);
+        std::cout << vector.back() << std::endl;
+        vector.pop_back();
+        std::cout << vector.size() << std::endl;
+        vector.push_back(3);
+        vector.push_back(5);
+        vector.push_back(737);
+        vector.push_back(0);
+        std::vector<int>::iterator it = vector.begin();
+        std::vector<int>::iterator ite = vector.end();
+        ++it;
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::vector<int> s(vector);
+        std::cout << "---------after copy" <<std::endl;
+        std::vector<int>::iterator _it = s.begin();
+        std::vector<int>::iterator _ite = s.end();
+        while (_it != _ite)
+        {
+            std::cout << *_it << std::endl;
+            ++_it;
+        }
     }
     return 0;
 }
